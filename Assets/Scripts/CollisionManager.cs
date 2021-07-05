0c08b9667e3a2980fb9d8ad5d599c78f1825ae11
Petra Béczi <beczi@student.chalmers.se>
2021-07-05 14:11:59 +0200

    Startup the project

---
+++ Assets/Scripts/CollisionManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "HexCollider")
+        {
+            //play particles
+        }
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "HexCollider")
+        {
+            //play particles
+            //Decrease health by seconds
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "HexCollider")
+        {
+            //stop playing particles
+        }
+    }
+}
