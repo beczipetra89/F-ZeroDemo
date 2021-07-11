45039646cfdfb3bf57efffc56782ca527f1a52db
Petra Béczi <beczi@student.chalmers.se>
2021-07-11 12:14:28 +0200

    Deducting power (health system)

--- Assets/Scripts/CollisionManager.cs
+++ Assets/Scripts/CollisionManager.cs
@@ -1,29 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollisionManager : MonoBehaviour
 {
+    public float health;
+    public Slider powerSlider;
+
+    void Update()
+    {
+        powerSlider.value = health;
+    }
+
     void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "HexCollider")
+        
+            if (other.gameObject.tag == "Hex_Coll_R" || other.gameObject.tag == "Hex_Coll_L")
         {
+            // Decrease health
+
+           // health = health - 10f;
             //play particles
         }
     }
 
-    void OnCollisionStay(Collision other)
+    void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.tag == "HexCollider")
+        if (other.gameObject.tag == "Hex_Coll_R" || other.gameObject.tag == "Hex_Coll_L")
         {
             //play particles
-            //Decrease health by seconds
+            //Decrease health continuously
+            health -= 10f * Time.deltaTime;
         }
     }
 
-    void OnCollisionExit(Collision other)
+    void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "HexCollider")
+        if (other.gameObject.tag == "Hex_Coll_R" || other.gameObject.tag == "Hex_Coll_L")
         {
             //stop playing particles
         }
--- Assets/Scripts/RaceManager.cs
+++ Assets/Scripts/RaceManager.cs
@@ -9,18 +9,21 @@ public class RaceManager : MonoBehaviour
 {
     bool racestarted = false;
     bool countDownFinished = false;
-   
+
     GameObject[] AICars;
+    public GameObject Player;
 
     float currentTime = 0f;
     float startingTime = 4f;
 
     public TextMeshProUGUI countdownText;
+    public TextMeshProUGUI goTxt;
 
     void Start()
     {
+        goTxt.enabled = false;
         AICars = GameObject.FindGameObjectsWithTag("AiCar");
-        foreach(GameObject car in AICars)
+        foreach (GameObject car in AICars)
         {
             car.GetComponent<CarAIControl>().enabled = false;
         }
@@ -33,19 +36,35 @@ public class RaceManager : MonoBehaviour
         currentTime -= 1 * Time.deltaTime;
         countdownText.text = currentTime.ToString("0");
 
-        if (currentTime <= 0)
+        if (currentTime <= 1)
         {
-            currentTime = 0;
             countDownFinished = true;
+            countdownText.enabled = false;
         }
+
 
         if (!racestarted && countDownFinished)
         {
-            foreach(GameObject car in AICars)
+            foreach (GameObject car in AICars)
             {
                 car.GetComponent<CarAIControl>().enabled = true;
             }
             racestarted = true;
+            goTxt.enabled = true;
         }
+
+        if (goTxt.enabled)
+        {
+             StartCoroutine(ExampleCoroutine());
+        }
+
     }
+
+
+    private IEnumerator ExampleCoroutine()
+    {
+        yield return new WaitForSeconds(2);
+        goTxt.enabled = false;
+    }
+
 }
