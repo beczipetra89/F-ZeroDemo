676b4bc584901b7981c11c44547725a3beec0a0c
Petra Béczi <beczi@student.chalmers.se>
2021-07-11 18:26:20 +0200

    Recharging VFX graphs

--- Assets/Scripts/CollisionManager.cs
+++ Assets/Scripts/CollisionManager.cs
@@ -7,39 +7,69 @@ public class CollisionManager : MonoBehaviour
 {
     public float health;
     public Slider powerSlider;
+    public GameObject damageParticle;
+    public GameObject rechargingParticles;
+
+    void Start()
+    {
+        damageParticle.SetActive(false);
+        rechargingParticles.SetActive(false);
+    }
 
     void Update()
     {
         powerSlider.value = health;
+
+        if(powerSlider.value == powerSlider.maxValue)
+        {
+            rechargingParticles.SetActive(false);
+        }
     }
 
-    void OnCollisionEnter(Collision other)
+    void OnTriggerEnter(Collider other)
     {
-        
-            if (other.gameObject.tag == "Hex_Coll_R" || other.gameObject.tag == "Hex_Coll_L")
+        //Play damage particles
+        if (other.gameObject.tag == "Hex_Coll_L" || other.gameObject.tag == "Hex_Coll_R")
         {
-            // Decrease health
+            damageParticle.SetActive(true);
+        }
 
-           // health = health - 10f;
-            //play particles
+        // Play charging particles
+        if (other.gameObject.tag == "Charging")
+        {
+            if (powerSlider.value <= powerSlider.maxValue)
+                rechargingParticles.SetActive(true);
         }
     }
 
     void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.tag == "Hex_Coll_R" || other.gameObject.tag == "Hex_Coll_L")
+        //DEDUCT HEALTH
+        if (other.gameObject.tag == "Hex_Coll_L" || other.gameObject.tag == "Hex_Coll_R")
         {
-            //play particles
             //Decrease health continuously
             health -= 10f * Time.deltaTime;
+        }
+
+        //INCREASE HEALTH
+        if (other.gameObject.tag == "Charging")
+        {
+            health += 10f * Time.deltaTime;
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Hex_Coll_R" || other.gameObject.tag == "Hex_Coll_L")
+        if (other.gameObject.tag == "Hex_Coll_L" || other.gameObject.tag == "Hex_Coll_R")
         {
-            //stop playing particles
+            //Stop playing particles
+            damageParticle.SetActive(false);
+        }
+
+        //Stop playing charging particles
+        if (other.gameObject.tag == "Charging")
+        {
+            rechargingParticles.SetActive(false);
         }
     }
 }
